d5244ac3c2c42e6d0e18deb6f15a573679217908
Franco Gatti <francogatti.sdp@outlook.com>
2024-08-08 17:20:40 -0300

    Selector de Vests y Fetch

--- Js/selectorsJs/vest-selector.js
+++ Js/selectorsJs/vest-selector.js
@@ -1,68 +1,36 @@
-const ClassBtn105 = document.getElementById('btn105')
-const ClassBtn106 = document.getElementById('btn106')
-const ClassBtn107 = document.getElementById('btn107')
-const ClassBtn108 = document.getElementById('btn108')
-const ClassBtn109 = document.getElementById('btn109')
 
 const vestShock = document.getElementById("vest-shock")
 const vestPen = document.getElementById("vest-penetration")
 const vestDmg = document.getElementById("vest-damage")
-
 let foundArmor
+let vests = []
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetch("../data/vests.json")
+        .then(response => response.json())
+        .then(data => {
+            function buscarChaleco(vestId) {
+                foundArmor = vests.flat().find(vest => vest.vestId === vestId);            }            
+            vests = data;
+            console.log(data)
+            vests.forEach(vest=> { 
+                const button = document.createElement('button')
+                button.id = `${vest.vestId}`;
+                button.innerText = `${vest.name}`
+                button.addEventListener
+                    ('click', function (event) {
+                        event.preventDefault()
+                        vestId = vest.vestId
+                        console.log(vest.vestId)
+                        buscarChaleco(vestId)
+                        console.log(foundArmor)
+                        vestShock.innerText = foundArmor.shockProtection
+                        vestPen.innerText = foundArmor.penetrationProtection
+                        vestDmg.innerText = foundArmor.damageProtection
+                    })
+                    vestSelector.appendChild(button);
+            })
 
-function buscarChaleco(vestsId) {
-    foundArmor = vests.flat().find(vest => vest.vestId === vestsId);
-}
-
-ClassBtn105.addEventListener('click', function (event) {
-    event.preventDefault()
-    let vestsId = 105
-    console.log(vestsId)
-    buscarChaleco(vestsId)
-    console.log(foundArmor)
-    vestShock.innerText = foundArmor.shockProtection
-    vestPen.innerText = foundArmor.penetrationProtection
-    vestDmg.innerText = foundArmor.damageProtection
+        })
 })
-ClassBtn106.addEventListener('click', function (event) {
-    event.preventDefault()
-    let vestsId = 106
-    console.log(vestsId)
-    buscarChaleco(vestsId)
-    console.log(foundArmor)
-    vestShock.innerText = foundArmor.shockProtection
-    vestPen.innerText = foundArmor.penetrationProtection
-    vestDmg.innerText = foundArmor.damageProtection
-})
-ClassBtn107.addEventListener('click', function (event) {
-    event.preventDefault()
-    let vestsId = 107
-    console.log(vestsId)
-    buscarChaleco(vestsId)
-    console.log(foundArmor)
-    vestShock.innerText = foundArmor.shockProtection
-    vestPen.innerText = foundArmor.penetrationProtection
-    vestDmg.innerText = foundArmor.damageProtection
-})
-ClassBtn108.addEventListener('click', function (event) {
-    event.preventDefault()
-    let vestsId = 108
-    console.log(vestsId)
-    buscarChaleco(vestsId)
-    console.log(foundArmor)
-    vestShock.innerText = foundArmor.shockProtection
-    vestPen.innerText = foundArmor.penetrationProtection
-    vestDmg.innerText = foundArmor.damageProtection
-
-})
-ClassBtn109.addEventListener('click', function (event) {
-    event.preventDefault()
-    let vestsId = 109
-    console.log(vestsId)
-    buscarChaleco(vestsId)
-    console.log(foundArmor)
-    vestShock.innerText = foundArmor.shockProtection
-    vestPen.innerText = foundArmor.penetrationProtection
-    vestDmg.innerText = foundArmor.damageProtection
-})
+const vestSelector=document.getElementById('vest-selector')
--- data/data.js
+++ data/data.js
@@ -1,93 +1,93 @@
-// Efectos de la distancia
-const distance1 = {
-    meters: "100 a 200 metros.",
-    distanceObjId: 155,
-    reducedShock: 1,
-    reducedPenetration: 1,
-    reducedDamage: 0.1,
-}
-const distance2 = {
-    meters: "200 a 300 mts.",
-    distanceObjId: 156,
-    reducedShock: 1.5,
-    reducedPenetration: 1.3,
-    reducedDamage: 1.4,
-}
-const distance3 = {
-    meters: "300 a 400 mts.",
-    distanceObjId: 157,
-    reducedShock: 1.8,
-    reducedPenetration: 1.5,
-    reducedDamage: 1.5,
-}
-const distance4 = {
-    meters: "400 a 500 mts.",
-    distanceObjId: 158,
-    reducedShock: 2,
-    reducedPenetration: 1.7,
-    reducedDamage: 1.7,
-}
-const distance5 = {
-    meters: "500 a 600 mts.",
-    distanceObjId: 159,
-    reducedShock: 2.4,
-    reducedPenetration: 2,
-    reducedDamage: 1.9,
-}
-const distance6 = {
-    meters: "600 a 700 mts.",
-    distanceObjId: 160,
-    reducedShock: 2.6,
-    reducedPenetration: 2.1,
-    reducedDamage: 2,
-}
-const distance7 = {
-    meters: "+700 mts.",
-    distanceObjId: 161,
-    reducedShock: 3,
-    reducedPenetration: 2.5,
-    reducedDamage: 2.3,
-}
+// // Efectos de la distancia
+// const distance1 = {
+//     meters: "100 a 200 metros.",
+//     distanceObjId: 155,
+//     reducedShock: 1,
+//     reducedPenetration: 1,
+//     reducedDamage: 0.1,
+// }
+// const distance2 = {
+//     meters: "200 a 300 mts.",
+//     distanceObjId: 156,
+//     reducedShock: 1.5,
+//     reducedPenetration: 1.3,
+//     reducedDamage: 1.4,
+// }
+// const distance3 = {
+//     meters: "300 a 400 mts.",
+//     distanceObjId: 157,
+//     reducedShock: 1.8,
+//     reducedPenetration: 1.5,
+//     reducedDamage: 1.5,
+// }
+// const distance4 = {
+//     meters: "400 a 500 mts.",
+//     distanceObjId: 158,
+//     reducedShock: 2,
+//     reducedPenetration: 1.7,
+//     reducedDamage: 1.7,
+// }
+// const distance5 = {
+//     meters: "500 a 600 mts.",
+//     distanceObjId: 159,
+//     reducedShock: 2.4,
+//     reducedPenetration: 2,
+//     reducedDamage: 1.9,
+// }
+// const distance6 = {
+//     meters: "600 a 700 mts.",
+//     distanceObjId: 160,
+//     reducedShock: 2.6,
+//     reducedPenetration: 2.1,
+//     reducedDamage: 2,
+// }
+// const distance7 = {
+//     meters: "+700 mts.",
+//     distanceObjId: 161,
+//     reducedShock: 3,
+//     reducedPenetration: 2.5,
+//     reducedDamage: 2.3,
+// }
 
-const distances = [distance1, distance2, distance3, distance4, distance5, distance6, distance7]
+// const distances = [distance1, distance2, distance3, distance4, distance5, distance6, distance7]
 
-//   Chalecos / Armadura
-const tier1Vest = {
-    tier: "Tier 1",
-    vestId: 105,
-    shockProtection: 2,
-    penetrationProtection: 2,
-    damageProtection: 1.5,
-}
-const tier2Vest = {
-    tier: "Tier 2",
-    vestId: 106,
-    shockProtection: 2.3,
-    penetrationProtection: 2.5,
-    damageProtection: 1.8,
-}
-const tier3Vest = {
-    tier: "Tier 3",
-    vestId: 107,
-    shockProtection: 2.6,
-    penetrationProtection: 3,
-    damageProtection: 2.5,
-}
-const tier4Vest = {
-    tier: "Tier 4",
-    vestId: 108,
-    shockProtection: 2.8,
-    penetrationProtection: 5,
-    damageProtection: 3.5,
-}
-const tier5Vest = {
-    tier: "Tier 5",
-    vestId: 109,
-    shockProtection: 3,
-    penetrationProtection: 6.5,
-    damageProtection: 5,
-}
-const vests = [tier1Vest, tier2Vest, tier3Vest, tier4Vest, tier5Vest]
+// //   Chalecos / Armadura
+// const tier1Vest = {
+//     tier: "Tier 1",
+//     vestId: 105,
+//     shockProtection: 2,
+//     penetrationProtection: 2,
+//     damageProtection: 1.5,
+// }
+// const tier2Vest = {
+//     tier: "Tier 2",
+//     vestId: 106,
+//     shockProtection: 2.3,
+//     penetrationProtection: 2.5,
+//     damageProtection: 1.8,
+// }
+// const tier3Vest = {
+//     tier: "Tier 3",
+//     vestId: 107,
+//     shockProtection: 2.6,
+//     penetrationProtection: 3,
+//     damageProtection: 2.5,
+// }
+// const tier4Vest = {
+//     tier: "Tier 4",
+//     vestId: 108,
+//     shockProtection: 2.8,
+//     penetrationProtection: 5,
+//     damageProtection: 3.5,
+// }
+// const tier5Vest = {
+//     tier: "Tier 5",
+//     vestId: 109,
+//     shockProtection: 3,
+//     penetrationProtection: 6.5,
+//     damageProtection: 5,
+// }
+// const vests = [tier1Vest, tier2Vest, tier3Vest, tier4Vest, tier5Vest]
 
 // Objeto Pistolas y Array
 // Calculadora de Daño InGame.
